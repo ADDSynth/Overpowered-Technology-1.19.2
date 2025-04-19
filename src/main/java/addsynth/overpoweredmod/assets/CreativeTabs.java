4c84b9c4fb9b7f9995e52204f2303c32f5305570
ADDSynth <addsynth@gmail.com>
2025-04-18 21:03:45 -0500

    Changed the Creative Tab's Icon back to a Celestial Gem

--- src/main/java/addsynth/overpoweredmod/assets/CreativeTabs.java
+++ src/main/java/addsynth/overpoweredmod/assets/CreativeTabs.java
@@ -1,20 +1,19 @@
 package addsynth.overpoweredmod.assets;
 
-import addsynth.overpoweredmod.game.reference.OverpoweredBlocks;
+import addsynth.overpoweredmod.game.reference.OverpoweredItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 // Creative Tab is kept in a seperate class because there were multiple creative tabs
 // at one point in the past. Just leave it as-is because it doesn't affect anything,
 // and doesn't give that much performance benefit if moved into an existing class.
-// Only move it if we need to rewrite the whole mod.
 public final class CreativeTabs {
 
   public static final CreativeModeTab creative_tab = new CreativeModeTab("overpowered")
   {
     @Override
     public final ItemStack makeIcon(){
-      return new ItemStack(OverpoweredBlocks.inverter.get(), 1);
+      return new ItemStack(OverpoweredItems.celestial_gem.get(), 1);
     }
   };
 
